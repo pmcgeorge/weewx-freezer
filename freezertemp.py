db2f2e6355548c517e0719ad25fce62fd2cb9476
pmcgeorge <pmcgeorge@users.noreply.github.com>
2018-12-27 08:55:08 -0700

    Update freezertemp.py
    Removed modprobe it is not needed in current raspian instead use     dtoverlay=w1-gpio in config.txt

--- freezertemp.py
+++ freezertemp.py
@@ -3,8 +3,6 @@ import glob
 import time
 from datetime import datetime
 
-os.system('modprobe w1-gpio')
-os.system('modprobe w1-therm')
 base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
